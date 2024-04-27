ffb4f6e7a1b075e2b7e07e5ae0cb78d9b8f7033f
ajrafisura <aj_rafisura@yahoo.com>
2024-04-27 13:29:49 +0800

    Added codes for Prep3 (guess the number from 1 to 100)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+        int guess = -1;
+        int i = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userInput1 = Console.ReadLine();
+            guess = int.Parse(userInput1);
+            i = i + 1;
+
+            if (magicNumber == guess)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (magicNumber>guess)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else
+            {
+                Console.WriteLine("Lower.");
+            } 
+        } while (magicNumber != guess);
+
+        Console.WriteLine($"Number of attempts: {i}");
+        
     }
 }

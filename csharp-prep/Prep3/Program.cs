17bcce6e3ed7757dbbc89b47bbe52e30746c9954
ajrafisura <aj_rafisura@yahoo.com>
2024-04-27 13:44:58 +0800

    Completed Prep3 code with stretch requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,32 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1,100);
-        int guess = -1;
-        int i = 0;
+        string answer = "yes";
         do
         {
-            Console.Write("What is your guess? ");
-            string userInput1 = Console.ReadLine();
-            guess = int.Parse(userInput1);
-            i = i + 1;
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,100);
+            int guess = -1;
+            int i = 0;
+            do
+            {
+                Console.Write("What is your guess? ");
+                string userInput1 = Console.ReadLine();
+                guess = int.Parse(userInput1);
+                i = i + 1;
 
-            if (magicNumber == guess)
-            {
-                Console.WriteLine("You guessed it!");
-            }
-            else if (magicNumber>guess)
-            {
-                Console.WriteLine("Higher.");
-            }
-            else
-            {
-                Console.WriteLine("Lower.");
-            } 
-        } while (magicNumber != guess);
+                if (magicNumber == guess)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (magicNumber>guess)
+                {
+                    Console.WriteLine("Higher.");
+                }
+                else
+                {
+                    Console.WriteLine("Lower.");
+                } 
+            } while (magicNumber != guess);
 
-        Console.WriteLine($"Number of attempts: {i}");
+            Console.WriteLine($"Number of attempts: {i}");
+            Console.Write("Do you want to play again? (YES/NO)");
+            answer = Console.ReadLine();
+            
+        } while (answer.ToLower() == "yes");
         
     }
 }

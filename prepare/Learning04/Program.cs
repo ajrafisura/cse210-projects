b7d342bdfc1f5f567c7c9722622b6238af34e5ef
ajrafisura <aj_rafisura@yahoo.com>
2024-05-16 00:43:15 +0800

    Added Learning 4 codes

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Aries", "World War II");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment math = new MathAssignment("Roberto Rodriguez","Fraction","7.3","8-19" );
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment write = new WritingAssignment("Mary Waters","European History","The Cause of World War II");
+        Console.WriteLine(write.GetSummary());
+        Console.WriteLine(write.GetWritingInformation());
+
     }
 }
